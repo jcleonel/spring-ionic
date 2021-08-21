768f578b2ea9b426527739ed58ffd960947b1096
jcleonel <jean.leonel.costa@gmail.com>
2021-08-21 12:53:23 -0300

    PUT, DELETE e GET para Cliente

--- backend/src/main/java/com/jcleonel/cursomc/services/ClienteService.java
+++ backend/src/main/java/com/jcleonel/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.jcleonel.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jcleonel.cursomc.domain.Cliente;
+import com.jcleonel.cursomc.dto.ClienteDTO;
 import com.jcleonel.cursomc.repositories.ClienteRepository;
+import com.jcleonel.cursomc.services.exceptions.DataIntegrityException;
 import com.jcleonel.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,10 +22,44 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage,String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return clienteRepository.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um cliente com pedidos!");
+		}
+
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
 	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
