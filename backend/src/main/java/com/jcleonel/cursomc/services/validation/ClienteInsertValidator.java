66c4add6666e4f416899126ae7fcb7d614ba431e
jcleonel <jean.leonel.costa@gmail.com>
2021-08-22 18:52:11 -0300

    Validacao customizada de email repetido

--- backend/src/main/java/com/jcleonel/cursomc/services/validation/ClienteInsertValidator.java
+++ backend/src/main/java/com/jcleonel/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,14 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jcleonel.cursomc.domain.Cliente;
 import com.jcleonel.cursomc.domain.enums.TipoCliente;
 import com.jcleonel.cursomc.dto.ClienteNewDTO;
+import com.jcleonel.cursomc.repositories.ClienteRepository;
 import com.jcleonel.cursomc.resources.exception.FieldMessage;
 import com.jcleonel.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 	
-		
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,7 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
-
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
