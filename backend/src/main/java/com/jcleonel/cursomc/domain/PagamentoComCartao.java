fc6013cafe4c82fa1278dcaf9a3853673f2141de
jcleonel <jean.leonel.costa@gmail.com>
2021-08-23 08:28:36 -0300

    Inserindo Pedido

--- backend/src/main/java/com/jcleonel/cursomc/domain/PagamentoComCartao.java
+++ backend/src/main/java/com/jcleonel/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.jcleonel.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.jcleonel.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
--- backend/src/main/java/com/jcleonel/cursomc/resources/PedidoResource.java
+++ backend/src/main/java/com/jcleonel/cursomc/resources/PedidoResource.java
@@ -1,11 +1,18 @@
 package com.jcleonel.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jcleonel.cursomc.domain.Pedido;
 import com.jcleonel.cursomc.services.PedidoService;
@@ -21,6 +28,13 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = pedidoService.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = pedidoService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentServletMapping().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
