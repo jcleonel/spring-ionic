53ec3acfc9b1599aae47243bda822a9c94847c13
jcleonel <jean.leonel.costa@gmail.com>
2021-08-27 14:09:13 -0300

    Adicionando senha ao Cliente e perfil de usuário

--- backend/src/main/java/com/jcleonel/cursomc/services/DBService.java
+++ backend/src/main/java/com/jcleonel/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.jcleonel.cursomc.domain.PagamentoComCartao;
 import com.jcleonel.cursomc.domain.Pedido;
 import com.jcleonel.cursomc.domain.Produto;
 import com.jcleonel.cursomc.domain.enums.EstadoPagamento;
+import com.jcleonel.cursomc.domain.enums.Perfil;
 import com.jcleonel.cursomc.domain.enums.TipoCliente;
 import com.jcleonel.cursomc.repositories.CategoriaRepository;
 import com.jcleonel.cursomc.repositories.CidadeRepository;
@@ -117,13 +118,18 @@ public class DBService {
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "matheus.silva.so123@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa da Silva", "matheussilvaso123@gmail.com", "58611369041", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("27311123", "32138393"));
+		cli2.addPerfil(Perfil.ADMIN);
+		Endereco e3 = new Endereco(null, "Rua 200", "987", "Arvore de Jambolão na porta", "Centro", "987524123", cli2, c4);
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
