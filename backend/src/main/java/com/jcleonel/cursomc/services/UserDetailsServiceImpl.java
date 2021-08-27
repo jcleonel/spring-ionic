f333900433f4c249db46a31cd3cee8210205c20a
jcleonel <jean.leonel.costa@gmail.com>
2021-08-27 16:10:09 -0300

    Implementando autenticacao e gerando token JWT

---
+++ backend/src/main/java/com/jcleonel/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.jcleonel.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jcleonel.cursomc.domain.Cliente;
+import com.jcleonel.cursomc.repositories.ClienteRepository;
+import com.jcleonel.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = clienteRepository.findByEmail(email);		
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
