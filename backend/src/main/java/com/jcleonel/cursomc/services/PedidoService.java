61ad71456808e0253a6cfb4cbfc3f6c3f231fb9c
jcleonel <jean.leonel.costa@gmail.com>
2021-08-28 16:17:28 -0300

    restrigindo conteudo de Cliente e Pedidos

--- backend/src/main/java/com/jcleonel/cursomc/services/PedidoService.java
+++ backend/src/main/java/com/jcleonel/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jcleonel.cursomc.domain.Cliente;
 import com.jcleonel.cursomc.domain.ItemPedido;
 import com.jcleonel.cursomc.domain.PagamentoComBoleto;
 import com.jcleonel.cursomc.domain.Pedido;
@@ -14,29 +18,25 @@ import com.jcleonel.cursomc.domain.enums.EstadoPagamento;
 import com.jcleonel.cursomc.repositories.ItemPedidoRepository;
 import com.jcleonel.cursomc.repositories.PagamentoRepository;
 import com.jcleonel.cursomc.repositories.PedidoRepository;
+import com.jcleonel.cursomc.security.UserSS;
+import com.jcleonel.cursomc.services.exceptions.AuthorizationException;
 import com.jcleonel.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
 
 	@Autowired
-	private PedidoRepository pedidoRepository;
-	
+	private PedidoRepository pedidoRepository;	
 	@Autowired
-	private BoletoService boletoService;
-	
+	private BoletoService boletoService;	
 	@Autowired
-	private PagamentoRepository pagamentoRepository;
-	
+	private PagamentoRepository pagamentoRepository;	
 	@Autowired
-	private ProdutoService produtoService;
-	
+	private ProdutoService produtoService;	
 	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
-	
+	private ItemPedidoRepository itemPedidoRepository;	
 	@Autowired
-	private ClienteService clienteService;
-	
+	private ClienteService clienteService;	
 	@Autowired
 	private EmailService emailService;
 	
@@ -68,6 +68,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 	
 }
