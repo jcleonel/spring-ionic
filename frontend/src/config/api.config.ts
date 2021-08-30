e2917936b2a81f15f033b65c39581bcec2476cfc
jcleonel <jean.leonel.costa@gmail.com>
2021-08-30 16:36:06 -0300

    Mostrando as categorias

--- frontend/src/config/api.config.ts
+++ frontend/src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-   baseUrl: "https://spring-ionic-jc.herokuapp.com" 
+   baseUrl: "https://spring-ionic-jc.herokuapp.com" ,
+   bucketBaseUrl: "https://curso-spring-ionic-jean.s3.sa-east-1.amazonaws.com"
 }
--- frontend/src/pages/categorias/categorias.ts
+++ frontend/src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
@@ -16,6 +18,9 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  items: CategoriaDTO[];
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -23,7 +28,14 @@ export class CategoriasPage {
   }
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(response => console.log(response), error => console.log(error));
+    this.categoriaService.findAll()
+      .subscribe(response => {
+        this.items = response;
+      }, 
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
