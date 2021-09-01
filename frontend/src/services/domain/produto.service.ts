a33760c26e6eabd535488ce52749bc003fadcdd2
jcleonel <jean.leonel.costa@gmail.com>
2021-09-01 14:52:31 -0300

    Criando pagina Produtos

---
+++ frontend/src/services/domain/produto.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+import { API_CONFIG } from "../../config/api.config";
+
+@Injectable()
+export class ProdutoService {
+
+    constructor(public http: HttpClient) {
+    
+    }
+
+    findByCategoria(categoria_id: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, {responseType : 'blob'});
+      } 
+}
