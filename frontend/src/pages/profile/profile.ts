77e284baef7396f61f6f0ea551407d74d3226b43
jcleonel <jean.leonel.costa@gmail.com>
2021-09-02 15:51:14 -0300

    Fazendo upload da foto

--- frontend/src/pages/profile/profile.ts
+++ frontend/src/pages/profile/profile.ts
@@ -26,6 +26,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -37,9 +41,9 @@ export class ProfilePage {
           if (error.status == 403) {
             this.navCtrl.setRoot('HomePage');
           }
-        });
-    }
-    else {
+        }
+      );
+    } else {
       this.navCtrl.setRoot('HomePage');
     }
   }
@@ -69,4 +73,19 @@ export class ProfilePage {
     }, (err) => {
     });
   }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {
+      });
+  }
+
+  cancel() {
+    this.picture = null;
+  }
+
 }
